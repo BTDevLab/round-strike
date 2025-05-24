48a6d50103e7f03fdfcb9846bb4e75ad2ba6c363
Bruno Ferreira <brunocmferreira.dev@gmail.com>
2025-05-23 23:46:05 -0300

    :sparkles: feat: Implement JWT authentication in LoginUser handler

--- backend/internal/handler/user.go
+++ backend/internal/handler/user.go
@@ -2,13 +2,22 @@ package handler
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/tarikcarvalho08/round-strike/backend/db"
 	"github.com/tarikcarvalho08/round-strike/backend/internal/models"
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/gin-gonic/gin"
 )
+
+var jwtKey = []byte("chave-secreta")
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
 
 func CreateUser(c *gin.Context) {
 	var input struct {
@@ -49,14 +58,25 @@ func LoginUser(c *gin.Context) {
 
 	var user models.User
 	if err := db.DB.Where("username = ?", input.Username).First(&user).Error; err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
 		return
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(input.Password)); err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid password"})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+	expirationTime := time.Now().Add(1 * time.Hour)
+	claims := &Claims{
+		Username: user.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, _ := token.SignedString(jwtKey)
+
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
