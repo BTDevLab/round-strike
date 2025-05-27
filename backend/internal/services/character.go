8b10e19e29c2772e0ddd6ca5751b6795ab1afdc5
tarikcarvalho <tarik.carvalho08@gmail.com>
2025-05-27 18:38:14 +0100

    :recycle: refactor: Rename the agility property to dexterity, and XP to experience. Also rename Gorm returning values on a request (eg. int to intelligence)

--- backend/internal/services/character.go
+++ backend/internal/services/character.go
@@ -6,15 +6,15 @@ import "github.com/tarikcarvalho08/round-strike/backend/internal/models"
 // In future if add name validation, etc, will need to implement those in here and in the handler
 func GenerateCharacterDefaultStats(character models.Character) models.Character {
 	return models.Character{
-		Name:  character.Name,
-		Level: 1,
-		XP:    0,
-		HP:    100,
-		MP:    100,
+		Name:       character.Name,
+		Level:      1,
+		Experience: 0,
+		HP:         100,
+		MP:         100,
 		CharacterStats: models.CharacterStats{
 			Strength:     1,
 			Intelligence: 1,
-			Agility:      1,
+			Dexterity:    1,
 			Defense:      1,
 			MaxHP:        100,
 			MaxMP:        100,
