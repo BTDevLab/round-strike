6c73531fa5ba2251fd940b4d88862c4c43f8f755
Bruno Ferreira <brunocmferreira.dev@gmail.com>
2025-06-19 10:34:26 -0300

    :recycle: refactor: extract token validation logic into checkLogin function in ProtectedRoute component

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,31 +1,37 @@
 'use client';
 
-import { useEffect } from "react";
-import { useUserStore } from "@/stores/user";
-import { jwtDecode } from "jwt-decode";
-import type { User } from "@/types/user";
+import { useUserStore } from '@/stores/user';
+import type { User } from '@/types/user';
+import { jwtDecode } from 'jwt-decode';
+import { useEffect } from 'react';
+
+function checkLogin(setUser: (user: User | null) => void) {
+  const token = localStorage.getItem('token');
+  if (token) {
+    try {
+      const decodedUser = jwtDecode<User & { exp: number }>(token);
+      const isExpired = decodedUser.exp * 1000 < Date.now();
+      if (isExpired) {
+        localStorage.removeItem('token');
+        setUser(null);
+        return;
+      }
+      setUser(decodedUser);
+    } catch (error) {
+      console.error('Invalid token:', error);
+      localStorage.removeItem('token');
+      setUser(null);
+    }
+  } else {
+    setUser(null);
+  }
+}
 
 export function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const setUser = useUserStore((state) => state.setUser);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      try {
-        const decodedUser = jwtDecode<User & { exp: number }>(token);
-        
-        const isExpired = decodedUser.exp * 1000 < Date.now();
-
-        if(isExpired){
-            localStorage.removeItem("token");
-            return;
-        }
-        setUser(decodedUser);
-      } catch (error) {
-        console.error("Invalid token:", error);
-        localStorage.removeItem("token");
-      }
-    }
+    checkLogin(setUser);
   }, [setUser]);
 
   return <>{children}</>;
