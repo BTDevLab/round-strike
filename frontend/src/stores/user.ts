018f4d071f4a37d6bb474557477db9894ac8e285
Bruno Ferreira <brunocmferreira.dev@gmail.com>
2025-06-19 10:33:33 -0300

    :recycle: refactor: update setUser method to accept null user type in user store

--- frontend/src/stores/user.ts
+++ frontend/src/stores/user.ts
@@ -3,15 +3,15 @@ import { create } from "zustand";
 
 type UserStore = {
   user: User | null;
-  setUser: (user: User) => void;
+  setUser: (user: User | null) => void;
   logout: () => void;
 };
 
 export const useUserStore = create<UserStore>((set) => ({
   user: null,
-  setUser: (user: User) => set({ user }),
+  setUser: (user: User | null) => set({ user }),
   logout: () => {
     localStorage.removeItem("token");
-    set({ user: null});
-  }
+    set({ user: null });
+  },
 }));
