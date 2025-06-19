44232c35b29fa79490cd71acbf2ce72fcac1c6ac
Bruno Ferreira <brunocmferreira.dev@gmail.com>
2025-06-19 10:59:27 -0300

    :heavy_plus_sign: feat: change Character Page to DashboardPage

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-export default function LoginPage() {
+export default function DashboardPage() {
   return (
     <div className="w-full bg-muted flex flex-col items-center justify-center p-6 md:p-10 bg-gradient-to-b from-slate-900 via-purple-900 to-slate-900">
-      Character Page
+      Dashboard Page
     </div>
   );
 }
